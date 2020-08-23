1f5632af6057ccfcb52c3eed51221cbce440aace
kfdykme <wim.k.f@live.com>
2020-08-24 03:09:06 +0800

    [windows] change window width and default x
    Signed-off-by: kfdykme <wim.k.f@live.com>

--- windows/runner/window_configuration.cpp
+++ windows/runner/window_configuration.cpp
@@ -1,7 +1,9 @@
 #include "window_configuration.h"
-
+#include <windows.h>
+int  screenWidth   =   GetSystemMetrics(   SM_CXSCREEN   );
+int  screenHeight   =   GetSystemMetrics(   SM_CYSCREEN   );
 const wchar_t* kFlutterWindowTitle = L"denkuitop";
-const unsigned int kFlutterWindowOriginX = 10;
-const unsigned int kFlutterWindowOriginY = 10;
-const unsigned int kFlutterWindowWidth = 1280;
+const unsigned int kFlutterWindowWidth = 350;
 const unsigned int kFlutterWindowHeight = 720;
+const unsigned int kFlutterWindowOriginX = (screenWidth - kFlutterWindowWidth) / 2;
+const unsigned int kFlutterWindowOriginY = 10;
