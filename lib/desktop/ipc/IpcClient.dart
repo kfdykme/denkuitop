68f5f5315dc8cd7b92a50b3b4aff5c2b742e7772
kfdykme <wim.k.f@live.com>
2022-03-26 17:27:52 +0800

    fix crash on socket error

--- lib/desktop/ipc/IpcClient.dart
+++ lib/desktop/ipc/IpcClient.dart
@@ -1,7 +1,9 @@
 import 'dart:collection';
 
 import 'package:web_socket_channel/io.dart';
+import 'dart:async';
 
+import 'package:web_socket_channel/web_socket_channel.dart';
 class IpcClient {
   var mWebSocket;
 
@@ -15,13 +17,11 @@ class IpcClient {
 //    init();
   }
 
-  init() async {
-    if (inited) {
-      // print("IpcClient has already inited");
-      return;
-    }
-    mWebSocket = IOWebSocketChannel.connect("ws://127.0.0.1:8082",
-        headers: {'Origin': 'http://127.0.0.1'});
+  initSocket() {
+    mWebSocket = IOWebSocketChannel.connect("ws://127.0.0.1:8082", headers:  {
+                  'Origin':'http://127.0.0.1'
+                });
+
     print("IpcClient init");
     inited = true;
     mWebSocket.stream.listen((message) {
@@ -33,6 +33,24 @@ class IpcClient {
       if (callbacks["onmessage"] != null) callbacks["onmessage"](message);
     });
     this.send("DENKUI_START");
+  }
+
+  init() async {
+    if (inited) {
+      // print("IpcClient has already inited");
+      return;
+    }
+    Timer.periodic(const Duration(milliseconds: 1000), (timer) {
+        print("Try Connect Socket");
+        try {
+
+            this.initSocket();
+            timer.cancel();
+        } on WebSocketChannelException catch(e) {
+          print("Try Connect Socket fail" + e.toString());
+        }
+    });
+
   }
 
   send(dynamic data) {
