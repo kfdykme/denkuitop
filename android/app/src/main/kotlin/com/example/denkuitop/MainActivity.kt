8785d9e6214dc1e4fd5b0ab1e52b79d999a2c2c5
kfdykme <wim.k.f@live.com>
2022-10-17 21:09:52 +0800

    [Feature] support android

---
+++ android/app/src/main/kotlin/com/example/denkuitop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.denkuitop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/docbee/DocBeeHomePage.dart
@@ -0,0 +1,10 @@
+import 'package:denkuitop/remote/base/BaseRemotePage.dart';
+
+class KfToHomePage extends BaseRemotePage {
+  @override
+  BaseRemotePageState createState() {
+    return KfToHomeState();
+  }
+}
+
+class KfToHomeState extends BaseRemotePageState {}
